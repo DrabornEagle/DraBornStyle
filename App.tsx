be8999ef1e4277039ba7f65071b06d453c06d75b
DrabornEagle <draborneagle@gmail.com>
2026-06-27 01:59:41 +0300

    dkd_release_0_1_update_app_entry

--- App.tsx
+++ App.tsx
@@ -1,3 +1,12 @@
-export default function dkd_DraBornStyleApp() {
-  return null;
+const dkd_React = require('react');
+const dkd_react_native = require('react-native');
+
+function dkd_DraBornStyleApp() {
+  return dkd_React.createElement(
+    dkd_react_native.View,
+    null,
+    dkd_React.createElement(dkd_react_native.Text, null, 'DraBornStyle v0.1')
+  );
 }
+
+export default dkd_DraBornStyleApp;
