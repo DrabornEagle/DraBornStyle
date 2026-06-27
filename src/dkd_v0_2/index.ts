72e4e54c6230d95e1690f5713a72b9c4b5676023
DrabornEagle <draborneagle@gmail.com>
2026-06-27 20:09:25 +0300

    Export v0.2 admin payment tools

--- src/dkd_v0_2/index.ts
+++ src/dkd_v0_2/index.ts
@@ -9,3 +9,7 @@ export {
 } from './dkd_transaction_api';
 
 export { useDkdTransactions } from './use_dkd_transactions';
+
+export { DkdAdminPaymentPanel } from './dkd_admin_payment_panel';
+export type { DkdAdminPaymentApproval } from './dkd_admin_payment_api';
+export { dkdApprovePayment, dkdLoadPaymentApprovals, dkdRejectPayment } from './dkd_admin_payment_api';
