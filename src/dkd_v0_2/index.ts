3de5923dbf86dced8d6fffea26797fdaa3590d7b
DrabornEagle <draborneagle@gmail.com>
2026-06-27 22:06:27 +0300

    Export v0.2 QR and discount tools

--- src/dkd_v0_2/index.ts
+++ src/dkd_v0_2/index.ts
@@ -13,3 +13,5 @@ export { useDkdTransactions } from './use_dkd_transactions';
 export { DkdAdminPaymentPanel } from './dkd_admin_payment_panel';
 export type { DkdAdminPaymentApproval } from './dkd_admin_payment_api';
 export { dkdApprovePayment, dkdLoadPaymentApprovals, dkdRejectPayment } from './dkd_admin_payment_api';
+
+export { DkdBusinessV02ToolsPanel } from './dkd_business_v02_tools_panel';
